179fe98792bce490ee434de27c259e31be11bdc5
Andrew Kitlowski <kittechsix@gmail.com>
2026-03-04 08:46:37 -0600

    Add consult-wizard.css to app.html, bump SW v123

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
 // Network-first for code, cache-first for images
 // Ensures updates load immediately without manual cache clearing
 
-const CACHE_NAME = 'medkitt-v122';
+const CACHE_NAME = 'medkitt-v123';
 
 const ASSETS_TO_CACHE = [
   './',
