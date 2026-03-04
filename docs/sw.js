4e7ad7f9160693f45b70b699f62e73ba9eb89b29
Andrew Kitlowski <kittechsix@gmail.com>
2026-03-04 07:58:34 -0600

    Bump SW cache v122 - force reload ConsultWizard

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
 // Network-first for code, cache-first for images
 // Ensures updates load immediately without manual cache clearing
 
-const CACHE_NAME = 'medkitt-v121';
+const CACHE_NAME = 'medkitt-v122';
 
 const ASSETS_TO_CACHE = [
   './',
