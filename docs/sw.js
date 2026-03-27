b2b1a45164d1c0ebd94b024d24a962087e2c9df9
kittech <kittechsix@gmail.com>
2026-03-27 13:41:21 -0500

    Bump DATA_VERSION 85, SW v321

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
 // Network-first for code, cache-first for images
 // Ensures updates load immediately without manual cache clearing
 
-const CACHE_NAME = 'medkitt-v320';
+const CACHE_NAME = 'medkitt-v321';
 
 const ASSETS_TO_CACHE = [
   './',
