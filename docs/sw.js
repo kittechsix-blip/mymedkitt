f0bc74fcddbb61ac47b39c304f95cc72a148bed4
kittech <kittechsix@gmail.com>
2026-03-26 21:58:13 -0500

    Deploy: bump cache to v304

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
 // Network-first for code, cache-first for images
 // Ensures updates load immediately without manual cache clearing
 
-const CACHE_NAME = 'medkitt-v303';
+const CACHE_NAME = 'medkitt-v304';
 
 const ASSETS_TO_CACHE = [
   './',
