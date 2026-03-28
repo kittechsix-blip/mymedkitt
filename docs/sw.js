97b39cde9adda9198b54a86884f7f9cefcf2bf8e
kittech <kittechsix@gmail.com>
2026-03-28 06:09:23 -0500

    Update DKA BOHB severity threshold to 2024 ADA/EASD/JBDS consensus (>6 mmol/L)

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
 // Network-first for code, cache-first for images
 // Ensures updates load immediately without manual cache clearing
 
-const CACHE_NAME = 'medkitt-v324';
+const CACHE_NAME = 'medkitt-v325';
 
 const ASSETS_TO_CACHE = [
   './',
