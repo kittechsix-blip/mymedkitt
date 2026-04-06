5c5f3ee0b6c612b989a84bb864d0894eaa934005
kittech <kittechsix@gmail.com>
2026-04-06 10:30:43 -0500

    Bump DATA_VERSION 172 + SW v421 for CSS toggle fix

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@
 // Network-first for code, cache-first for images
 // Ensures updates load immediately without manual cache clearing
 
-const CACHE_NAME = 'medkitt-v420';
+const CACHE_NAME = 'medkitt-v421';
 
 const ASSETS_TO_CACHE = [
   './',
